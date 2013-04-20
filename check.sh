c4d29933415bffac526b246a160d22befdec19a0
Patrick Adair <padair@he.net>
2013-04-20 01:11:32 -0700

    Repo creation.

---
+++ check.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+# Print out (eventually email) every time ran; if executed more than
+# five times in five minutes, then print out (/email) an additional
+# message.
+echo "Hey! Someone ran me!"

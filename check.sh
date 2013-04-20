af5a6fea2d766b084e2af571b320a3d37a64980c
Patrick Adair <padair@he.net>
2013-04-20 01:42:43 -0700

    Working script that just prints out.
    Need to add in emails as requested.

--- check.sh
+++ check.sh
@@ -2,4 +2,31 @@
 # Print out (eventually email) every time ran; if executed more than
 # five times in five minutes, then print out (/email) an additional
 # message.
+
+# Configs.
+STATUS_FILE="./.status"
+TS=`date +%s`
+
 echo "Hey! Someone ran me!"
+if [[ ! -f $STATUS_FILE ]];
+then
+    # Store the time of each run so that subsequent runs can determine
+    # their behaviour.
+    echo "1 $TS" > $STATUS_FILE
+else
+    # We base our 5-minutes off of when the file was created, which
+    # will be the first entry in the list.
+    ETIME=$((`date +%s`-300))
+    CTIME=$((`head -n 1 $STATUS_FILE | awk '{ print $2 }'`))
+    if [ "$CTIME" -lt "$ETIME" ];
+    then
+        echo "1 $TS" > $STATUS_FILE
+    else
+        RUNS=`tail -n 1 $STATUS_FILE | awk '{ print $1 }'`
+        if [ "$RUNS" -ge "5" ];
+        then
+            echo "Whoa there! Too many runs!"
+        fi
+        echo "$((RUNS+1)) $TS" >> $STATUS_FILE
+    fi
+fi

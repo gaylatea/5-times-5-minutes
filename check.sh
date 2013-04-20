201a18f382d9dcfdd5f32c4602f2b45aeb11bdf8
Patrick Adair <padair@he.net>
2013-04-20 01:51:42 -0700

    Send out emails to myself (although I should make this configurable) instead of just printing out.

--- check.sh
+++ check.sh
@@ -6,8 +6,14 @@
 # Configs.
 STATUS_FILE="./.status"
 TS=`date +%s`
+NORMAL_RUN_SUBJECT="A Normal Script Run."
+NORMAL_RUN_MAIL="arian@localhost"
+NORMAL_RUN_CONTENT="Here would be a report, or a status update."
+SPECIAL_RUN_SUBJECT="Whoa! Too Many Runs!"
+SPECIAL_RUN_MAIL="arian@localhost"
+SPECIAL_RUN_CONTENT="Someone's run the script too many times in 5 minutes."
 
-echo "Hey! Someone ran me!"
+echo "$NORMAL_RUN_CONTENT" | mail -s "$NORMAL_RUN_SUBJECT" "$NORMAL_RUN_MAIL"
 if [[ ! -f $STATUS_FILE ]];
 then
     # Store the time of each run so that subsequent runs can determine
@@ -26,6 +32,7 @@ else
         if [ "$RUNS" -ge "5" ];
         then
             echo "Whoa there! Too many runs!"
+            echo "$SPECIAL_RUN_CONTENT" | mail -s "$SPECIAL_RUN_SUBJECT" "$SPECIAL_RUN_MAIL"
         fi
         echo "$((RUNS+1)) $TS" >> $STATUS_FILE
     fi

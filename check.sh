e843af65f84e6fd651039cbad03a1796b7d4bc2a
Patrick Adair <padair@he.net>
2013-04-23 02:18:36 -0700

    Mack and James both reported an issue with the 5-minute window.
    This changeset should more accurately check the times the script has last executed,
    and base its decisions off of everything in the file instead of just the first
    line.

--- check.sh
+++ check.sh
@@ -11,6 +11,7 @@
 # Configs.
 STATUS_FILE="./.status"
 TS=`date -u +%s`
+THRESHOLD=300
 NORMAL_RUN_SUBJECT="A Normal Script Run."
 NORMAL_RUN_MAIL="arian@localhost"
 NORMAL_RUN_CONTENT="Here would be a report, or a status update."
@@ -18,30 +19,47 @@ SPECIAL_RUN_SUBJECT="Whoa! Too Many Runs!"
 SPECIAL_RUN_MAIL="arian@localhost"
 SPECIAL_RUN_CONTENT="Someone's run the script too many times in 5 minutes."
 
-echo "$NORMAL_RUN_CONTENT" | mail -s "$NORMAL_RUN_SUBJECT" "$NORMAL_RUN_MAIL"
+echo $NORMAL_RUN_CONTENT
+#echo "$NORMAL_RUN_CONTENT" | mail -s "$NORMAL_RUN_SUBJECT" "$NORMAL_RUN_MAIL"
 if [[ ! -f $STATUS_FILE ]];
 then
     # Store the time of each run so that subsequent runs can determine
     # their behaviour.
     echo "1 $TS" > $STATUS_FILE
 else
-    # We base our 5-minutes off of when the file was created, which
-    # will be the first entry in the list.
-    ETIME=$((`date -u +%s`-300))
-    CTIME=$((`head -n 1 $STATUS_FILE | awk '{ print $2 }'`))
+    # Look for a 5-minute interval from the last time the script was executed.
+    # Rewrite the file if too much time has passed.
+    ETIME=$((`date -u +%s`-$THRESHOLD))
+    CTIME=$((`tail -n 1 $STATUS_FILE | awk '{ print $2 }'`))
     if [ "$CTIME" -lt "$ETIME" ];
     then
         # Reset the status file to reflect that we're tracking a new
         # five-minute interval.
         echo "1 $TS" > $STATUS_FILE
     else
-        RUNS=`tail -n 1 $STATUS_FILE | awk '{ print $1 }'`
-        # Remember that if it's been executed 4 times already, this is
-        # the fifth and we should send out the special run email.
-        if [ "$RUNS" -ge "4" ];
+        LAST_RUN=`cat $STATUS_FILE | wc -l | awk '{ print $1 }'`
+
+        # So we know it hasn't been five minutes since the last run.
+        # Count how many runs HAVE happened in the last five minutes,
+        # and do our special line processing from that.
+        #
+        # This should alleviate the issue that Mack had reported.
+        PASTRUNS=0
+        while read line
+        do
+            RUNTIME=`echo $line | awk '{ print $2 }'`
+            if [ "$RUNTIME" -gt "$ETIME" ];
+            then
+                PASTRUNS=$((PASTRUNS+1))
+            fi
+        done < $STATUS_FILE
+
+        # 4 runs have already happened, so this is the fifth.
+        if [ "$PASTRUNS" -ge "4" ];
         then
-            echo "$SPECIAL_RUN_CONTENT" | mail -s "$SPECIAL_RUN_SUBJECT" "$SPECIAL_RUN_MAIL"
+            echo $SPECIAL_RUN_CONTENT
+            #echo "$SPECIAL_RUN_CONTENT" | mail -s "$SPECIAL_RUN_SUBJECT" "$SPECIAL_RUN_MAIL"
         fi
-        echo "$((RUNS+1)) $TS" >> $STATUS_FILE
+        echo "$((LAST_RUN+1)) $TS" >> $STATUS_FILE
     fi
 fi
